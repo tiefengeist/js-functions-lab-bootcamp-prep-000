fb447fadb5f013018fd97179e04f3a43ddd4f0ee
Sahir Nambiar <sahirnambiar@gmail.com>
2018-10-26 23:38:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function happyHolidayTo(holiday, name) {
  return `Happy ${holiday}, ${name}!`;
 }
 
-function holidayCountdown(days, nom) {
+function holidayCountdown(nom, days) {
  return  `It's ${days} days until ${nom}!`;
 }
